8b8969b3b157165e05246415f36daa2278fa9b8a
Tonanzi-Samuele <samuele.github@gmail.com>
2025-03-18 08:59:25 +0100

    es tpsi

--- TPSI/tonanzi_samuele/esercizi_tdd_STUDENTI/esercizio_js_62/soluzione.js
+++ TPSI/tonanzi_samuele/esercizi_tdd_STUDENTI/esercizio_js_62/soluzione.js
@@ -3,8 +3,8 @@ export const unique_random_numbers = (n, m) => {
    let arr = new Set()
 
    while (arr.size < n)
-      arr.add(Math.floor(Math.random()* m) + 1)
+      arr.add(Math.ceil(Math.random()* m))
 
-   return Array.from(arr).sort((a,b) => b - a )
+   return Array.from(arr).sort()
    
 }
